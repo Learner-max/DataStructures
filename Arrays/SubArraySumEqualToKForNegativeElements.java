7b26eb43803b54c106f9f0eb89aac18b79eec94a
Learner-max <72613347+Learner-max@users.noreply.github.com>
2020-11-06 03:49:57 +0530

    Update SubArraySumEqualToKForNegativeElements.java

--- Arrays/SubArraySumEqualToKForNegativeElements.java
+++ Arrays/SubArraySumEqualToKForNegativeElements.java
@@ -7,6 +7,7 @@ import java.io.*;
 /* Name of the class has to be "Main" only if the class is public. */
 class Codechef
 {
+	//Same program can be used to find Subarray with sum zero.
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
