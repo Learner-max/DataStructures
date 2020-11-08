517ee4b6c0cc15550e4136edceecdd0c989d0839
Learner-max <72613347+Learner-max@users.noreply.github.com>
2020-11-08 14:26:37 +0530

    Count the number of possible triangles
    Given an unsorted array of positive integers, find the number of triangles that can be formed with three different array elements as three sides of triangles. For a triangle to be possible from 3 values, the sum of any of the two values (or sides) must be greater than the third value (or third side).

---
+++ Arrays/Vaild Triangles in Array.java
@@ -0,0 +1,39 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// Valid Triangle number
+		
+		int[] arr={0,1,0,1};
+		//{6,7,8,10,12,14,50};
+		
+		validTriangleNumbers(arr);
+	}
+	
+	public static void validTriangleNumbers(int[] arr)
+	{
+	    int count=0;
+	    int n=arr.length;
+	    int k=0;
+	    for(int i=0;i<=n-3;i++)
+	    {
+	        k=i+2;
+	        for(int j=i+1;j<=n-2 && arr[i]!=0;j++)
+	        {
+	            while(k<n && arr[i]+arr[j]>arr[k])
+	            k++;
+	            
+	            count+=k-j-1;
+	        }
+	    }
+	    
+	    System.out.println("Count "+ count);
+	}
+}
