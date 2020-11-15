c59bc153e9d89e16fb513dfa5cef90fb70ab7e17
Learner-max <72613347+Learner-max@users.noreply.github.com>
2020-11-16 00:43:56 +0530

    Rotate Matrix by 90 degree in  single pass

--- Arrays/Rotate Matrix Elements by 90 degree.java
+++ Arrays/Rotate Matrix Elements by 90 degree.java
@@ -92,4 +92,26 @@ class Codechef
 	        System.out.println();
 	    }
 	}
+
+     // Rotate Matrix by 90 degree in  single pass
+	
+	public static void rotateMatrixInSinglePass(int[][] matrix)
+	{
+		int n=matrix.length;
+		
+		for(int i=0;i<(n/2+n%2);i++)
+		{
+			for(int j=0;j<n/2;j++)
+			{
+				int temp=matrix[n-1-j][i];
+				matrix[n-1-j][i]=matrix[n-1-i][n-1-j];
+				matrix[n-1-i][n-1-j]=matrix[j][n-1-i];
+				matrix[j][n-1-j]=matrix[i][j];
+				matrix[i][j]=temp;
+			}
+		}
+	}
+
+		   
+			    
 }
