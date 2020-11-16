9a6d1411867936af8bb6b2dadfdf014970793b14
Learner-max <72613347+Learner-max@users.noreply.github.com>
2020-11-16 17:44:27 +0530

    Given an array consisting 0's, 1's and 2's, write a algorithm to sort it
    Given an array A[] consisting 0s, 1s and 2s. The task is to write a function that sorts the given array. The functions should put all 0s first, then all 1s and all 2s in last.

---
+++ Arrays/Sort an array of 0s, 1s and 2s.java
@@ -0,0 +1,67 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// Sort an array of 0s, 1s and 2s
+		
+		/*
+		
+		Input: {0, 1, 2, 0, 1, 2}
+        Output: {0, 0, 1, 1, 2, 2}
+        
+        Input: {0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1}
+        Output: {0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2}
+
+      */
+       int[] arr={2,2};
+       sortColors(arr);
+	}
+	
+	public static void sortColors(int[] arr) {
+        
+            if(arr.length==0 || arr.length==1)
+                return;
+	        
+	        int l=0;
+	        int h=arr.length-1;
+	        int mid=0;
+	        
+	        while(mid<=h)
+	        {
+	            if(arr[mid]==0 )
+	            {
+	            	if(mid!=l)
+	            	{ int temp=arr[l];
+	                arr[l]=arr[mid];
+	                arr[mid]=temp;}
+	                l++;
+	                mid++;
+	            }else if(arr[mid]==1)
+	            {
+	                mid++;
+	            }else if(arr[mid]==2 )
+	            {
+	               if(arr[mid]!=arr[h])
+	              {
+	                int temp=arr[h];
+	                arr[h]=arr[mid];
+	                arr[mid]=temp;
+	              }
+	              
+	                h--;
+	            }
+	        }
+	        for(int a:arr)
+	        System.out.print(a+" ");
+	       
+	        
+	    }
+	
+}
