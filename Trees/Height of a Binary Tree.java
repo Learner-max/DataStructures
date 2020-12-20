b2eb5944826cf1ba73937a8ae71e727f6fc3f8a0
Learner-max <72613347+Learner-max@users.noreply.github.com>
2020-12-20 11:33:34 +0530

    Height of a Binary Tree
    Height of a Binary Tree

---
+++ Trees/Height of a Binary Tree.java
@@ -0,0 +1,137 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+    
+	static class Node
+    {
+        int data;
+        Node left;
+        Node right;
+        
+        Node(int data)
+        {
+            this.data=data;
+        }
+    }
+    
+    
+    static class BinaryTree
+    {
+        Node root;
+        
+        public void traversePreOrderRecursion(Node root)
+        {
+            if(root==null)
+                return;
+            
+            System.out.print(root.data+" ");
+            traversePreOrderRecursion(root.left);
+            traversePreOrderRecursion(root.right);
+               
+        }
+        
+        
+        public void traverseInOrderRecursion(Node root)
+        {
+            if(root==null)
+                return;
+            
+            
+            traverseInOrderRecursion(root.left);
+            System.out.print(root.data+" ");
+            traverseInOrderRecursion(root.right);
+               
+        }
+        
+        public void traversePostOrderRecursion(Node root)
+        {
+            if(root==null)
+                return;
+            
+            
+            traversePostOrderRecursion(root.left);
+            
+            traversePostOrderRecursion(root.right);
+            System.out.print(root.data+" ");
+               
+        }
+        
+       /*
+          Size of a Binary tree -Recursive
+       */
+       public int sizeOfTree(Node root)
+       {
+           if(root==null)
+           return 0;
+           
+           int ls=sizeOfTree(root.left);
+           int rs=sizeOfTree(root.right);
+           int res=ls+rs+1;
+           return res;
+       }
+       
+       
+       /*
+            Height of a Binary tree Recursive
+       */
+       public int height(Node root)
+       {
+           if(root==null)
+           return -1;;
+           
+           int lh=height(root.left);
+           int rh=height(root.right);
+           int th=Math.max(lh,rh)+1;
+           
+           return th;
+       }
+        /*
+            Height of a Binary tree Iterative
+       */
+       public int heightOfTree(Node root)
+       {
+           Queue<Node> q= new LinkedList<>();
+           q.add(root);
+           
+           int height=-1;//height of tree= level of tree
+           while(!q.isEmpty())
+           {
+               int size=q.size();
+               while(size!=0)
+               {
+                   Node temp=q.remove();
+                   if(temp.left!=null)
+                   q.add(temp.left);
+                   if(temp.right!=null)
+                   q.add(temp.right);
+                   
+                   size--;
+               }
+               height++;
+           }
+           
+           return height;
+       }
+    }
+    public static void main(String args[])
+    {
+        BinaryTree tree=new BinaryTree();
+        tree.root = new Node(1); 
+        tree.root.left = new Node(2); 
+        tree.root.right = new Node(3); 
+        tree.root.left.left = new Node(4); 
+        tree.root.left.right = new Node(5); 
+        
+       
+        //System.out.println(tree.sizeOfTree(tree.root));
+        //System.out.println(tree.sizeOfTreeIterative(tree.root));
+        System.out.println(tree.height(tree.root));
+        System.out.println(tree.heightOfTree(tree.root));
+    }
+}
