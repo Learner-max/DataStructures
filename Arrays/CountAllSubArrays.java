cafcfad0bcb095b7db290d357bb7f0fad726d179
Learner-max <72613347+Learner-max@users.noreply.github.com>
2021-05-06 04:00:22 +0530

    Update CountAllSubArrays.java

--- Arrays/CountAllSubArrays.java
+++ Arrays/CountAllSubArrays.java
@@ -17,7 +17,7 @@ class Codechef
 		countSubArrays(arr, sum);
 	}
 	
-	public static void countSubArrays(int[] arr , int sum)
+	/*public static void countSubArrays(int[] arr , int sum)
 	{
 	    HashMap<Integer, Integer> map=new HashMap<>();
 	    int curr_sum=0;
@@ -38,5 +38,42 @@ class Codechef
 	    }
 	    
 	    System.out.println(count);
+	}*/
+	
+	//////OR Can be done as below:
+	public static void countSubArrayWithSumK(int[] arr,int target)
+	{
+	    int count=0;
+	    if(arr.length==0)
+	       {
+	           System.out.println("No SubArray");
+	           return;
+	       }
+	       
+	   HashMap<Integer,Integer> map=new HashMap<>();
+	   int currsum=0;
+	   for(int i=0;i<arr.length;i++)
+	   {
+	      currsum+=arr[i];
+	      if(currsum==target)
+	      {
+	          count++;
+	         
+	      }
+	      if(map.containsKey(currsum-target))
+	      {
+	         //map.put(currsum-target,map.get(currsum-target)+1);
+	          count=count+map.get(currsum-target);
+	          
+	      }
+	      if(map.containsKey(currsum))
+	      map.put(currsum,map.get(currsum)+1);
+	      else
+	      map.put(currsum,1);
+	   }
+	   if(count==0)
+	   System.out.println("No SubArray with target sum found ");
+	   else
+	   System.out.println(count);
 	}
 }
