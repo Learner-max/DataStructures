f35a7411db012ebeaa541c4bd5f6a8bf403a3ab4
Learner-max <72613347+Learner-max@users.noreply.github.com>
2021-06-03 08:11:57 +0530

    Update Check Majority element in Sorted array.java

--- Check Majority element in Sorted array.java
+++ Check Majority element in Sorted array.java
@@ -37,4 +37,43 @@ class Codechef
 	    return (l+arr.length/2)<arr.length && arr[l+arr.length/2]==target;
 	    
 	}
+	/* 
+	  New Code- Refer This
+	  public static int binarySearch(int[] arr, int target)
+	{
+	    int l=0;
+	    int  r= arr.length-1;
+	    while(l<=r)
+	    {
+	        int mid=l+(r-l)/2;
+	        if((mid==0 ||(target>arr[mid-1]) && arr[mid]==target))
+	        return mid;
+	        if(arr[mid]<target)
+	        {
+	            l=mid+1;
+	        }else
+	            r=mid;
+	    }
+
+		return -1;
+	}
+	
+	public static boolean isMajority(int[] arr, int target)
+	{
+		if(arr.length==0 || arr ==null)
+			return false;
+	    int index=binarySearch(arr,target);
+	    System.out.println(index);
+	    int n=arr.length/2;
+	    if(index==-1)
+	    return false;
+	    if(arr[index]==arr[index+n])
+	    return true;
+	    else
+	    return false;
+	    
+	  
+	    
+	}
+	*/
 }
