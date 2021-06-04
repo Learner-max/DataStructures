044bc4835363cfabff6fb20338c073c2daa08898
Learner-max <72613347+Learner-max@users.noreply.github.com>
2021-06-04 14:31:02 +0530

    Update Intersection of three sorted arrays.java

--- Arrays/Intersection of three sorted arrays.java
+++ Arrays/Intersection of three sorted arrays.java
@@ -32,6 +32,7 @@ class Codechef
      
      commonElements(ar1,ar2,ar3);
 }
+	/*  Below method works if arrays dont have duplicates*/
     public static void commonElements(int[] ar1, int[] ar2, int[] ar3)
     {
         int i=0;int j=0; int k=0;
@@ -55,6 +56,65 @@ class Codechef
             {
                 k++;
             }
+        }
+    }
+	/*  To Handle duplicate elements present in arrays*/
+	public static void commonElementsWithDuplicates(int[] ar1, int[] ar2, int[] ar3)
+    {
+        int i=0;int j=0; int k=0;
+        int prev1=0;
+        int prev2=0;
+        int prev3=0;
+        int n1=ar1.length;
+        int n2=ar2.length;
+        int n3=ar3.length;
+        while(i< n1 && j<n2 && k<n3)
+        {
+              while(i<n1 && prev1==ar1[i] )
+                {
+                    i++;
+ 
+                }
+          
+            while(j<n2 && prev2==ar2[j])
+            {
+                
+                j++;
+                    
+            }
+            while(k<n3 && prev3==ar3[k])
+            {
+              
+                k++;
+                    
+            }
+            
+          if(i<n1 && j<n2 && k<n3){
+            if(ar1[i]==ar2[j] && ar2[j]==ar3[k])
+            {
+                System.out.print(ar1[i]+" ");
+                prev1=ar1[i];
+                prev2=ar2[j];
+                prev3=ar3[k];
+                i++;
+                j++;
+                k++;
+            }else if(ar1[i]<ar2[j])
+            {
+                prev1=ar1[i];
+                i++;
+            }else if(ar2[j]<ar3[k])
+            {
+                prev2=ar2[j];
+                j++;
+                
+            }else
+            {
+                prev3=ar3[k];
+                k++;
+            }
+          }
+           
         }
     }
 }
