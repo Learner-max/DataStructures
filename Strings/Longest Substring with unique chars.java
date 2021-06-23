d6719a0ca808f296fece32d50a914ff3955cc6a4
Learner-max <72613347+Learner-max@users.noreply.github.com>
2021-06-23 13:45:27 +0530

    Update Longest Substring with unique chars.java

--- Strings/Longest Substring with unique chars.java
+++ Strings/Longest Substring with unique chars.java
@@ -35,4 +35,34 @@ class Codechef
 	     return maxLen;
 	 }
 	
+	//TC of this approach O(n) cuz (Note: Even though we have nested for loops, start only goes from 0 to s.length() once
+	public int lengthOfLongestSubstring(String s) {
+       // System.out.println(s.trim().length());
+        if(s.length()==0)
+            return 0;
+        if(s.length()==1 || s.trim().length()==0)
+            return 1;
+        int[] count=new int[128];
+        int start=0;
+        int maxLen=Integer.MIN_VALUE;
+        for(int i=0;i<s.length();i++)
+        {
+            
+            count[s.charAt(i)]++;
+            if(count[s.charAt(i)]>1)
+            {
+                while(count[s.charAt(i)]>1)
+                {
+                    count[s.charAt(start)]--;
+                    start++;
+                }
+            }  
+            maxLen=Math.max(maxLen,(i-start+1));
+                
+            
+        }
+        
+        return maxLen;
+    }
+	
 }
